372aa79bb1d7e3d4871fc1c24337fb2d1afca969
aaldosari1 <dosr@live.com>
2021-12-04 16:55:17 +0300

    added the logic for 'SearchByName' component

--- src/SearchByName.js
+++ src/SearchByName.js
@@ -1,11 +1,11 @@
 import { useState,useEffect  } from "react";
 import axios from "axios";
-import SearchByNameData from "./Components/SearchByNameData";
+import CardSearch from "./Components/CardSearch";
 
 const SearchByName = () => {
 
   const [doctors,setDoctors] = useState([]);
-  const[name,setName] = useState([]);
+  const[name,setName] = useState("m");
 
   const getName= (e) => {
     setName(e.target.value)
@@ -23,27 +23,36 @@ const SearchByName = () => {
         console.log(err)
       });
   }, []);
-  
+  let n
   return (
     <>
+    <div className= "my_Container">
     {doctors.map((e)=>{
         //  { لما تكتب حاجة بالخانة الاولى حقت الاسم الاول ويحصل ويكون نفس الاسم موجد بالداتا بيس راح ينطبع الاسم من الكمبوننت اللي اسمه تحت}
          if(e.name===name){
-           return <SearchByNameData Data={name}/>
-           
-         }
-             
+           console.log(name); 
+         }   
              })}
             
       <form>
         <div className="form-searchbyname-container">
-          <input onChange={getName} placeholder="Enter first name " />
-          <input placeholder="Enter last name " />
-          <button >Search</button>
+        <div className="search-container">
+          <input id="name" placeholder="Enter doctor name " />
+          </div>
         </div>
       </form>
-      
+      <div>
+        {/* طلعت الزر برى الفورم لانه كان يسوي مشاكل جوه */}
+      <button  onClick={() => {
+            n = document.getElementById("name").value;
+            setName(n);
+            //console.log(n);
+          }}>Search</button>
 
+      </div>
+
+      {/* <CardSearch data={name}/>      الكمبوننت هذا يسوي خطأ لما تشيل الكومنت عنه */}
+      </div>
     </>
   );
 };
